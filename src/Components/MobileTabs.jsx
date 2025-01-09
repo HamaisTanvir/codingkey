44a3f0f45df396a6ba134060baaec97a3dffaed3
HamaisTanvir <hamais.tanvir@codingkey.com>
2025-01-09 19:04:22 +0500

    mobile tabs changes

--- src/Components/MobileTabs.jsx
+++ src/Components/MobileTabs.jsx
@@ -13,29 +13,24 @@ const MobileTabs = () => {
         </h1>
       </div>
 
-      <div className="w-full max-w-[92%] mx-auto">
-        {/* Tab Buttons */}
-        <div className="block md:hidden">
-          <div className="grid grid-cols-2 space-x-0 gap-y-4">
-            {/* Display two tabs */}
+      <div className="block md:hidden max-w-7xl mx-auto">
+          <div className="grid grid-cols-2 justify-items-center gap-y-4 px-6">
             <button 
-              className={`py-3 px-0 w-36 uppercase rounded-md border border-[#c3c3c3] text-[#696969] font-[500] text-[14px] transition-all duration-300 
+                className={`py-3 px-0 w-36 uppercase rounded-sm border border-[#c3c3c3] text-[#696969] font-[500] text-[14px] transition-all duration-300 
                 ${activeTab === 0 ? 'bg-[#43b7c5] text-white border-none' : 'bg-white'}
                 focus:bg-[#43b7c5] focus:text-white`}
-              onClick={() => setActiveTab(0)}
-            >
-              Mobile
+                onClick={() => setActiveTab(0)} >
+              Mobile 
             </button>
             <button
-              className={`py-3 px-0 w-36 uppercase rounded-md border border-[#c3c3c3] text-[#696969] font-[500] text-[14px] transition-all duration-300 
+                className={`py-3 px-0 w-36 uppercase rounded-sm border border-[#c3c3c3] text-[#696969] font-[500] text-[14px] transition-all duration-300 
                 ${activeTab === 1 ? 'bg-[#43b7c5] text-white border-none' : 'bg-white'}
                 focus:bg-[#43b7c5] focus:text-white`}
-              onClick={() => setActiveTab(1)}
-            >
+                onClick={() => setActiveTab(1)} >
               Frontend
             </button>
             <button
-              className={`py-3 px-0 w-36 uppercase rounded-md border border-[#c3c3c3] text-[#696969] font-[500] text-[14px] transition-all duration-300 
+              className={`py-3 px-0 w-36 uppercase rounded-sm border border-[#c3c3c3] text-[#696969] font-[500] text-[14px] transition-all duration-300 
                 ${activeTab === 2 ? 'bg-[#43b7c5] text-white border-none' : 'bg-white'}
                 focus:bg-[#43b7c5] focus:text-white`}
               onClick={() => setActiveTab(2)}
@@ -43,7 +38,7 @@ const MobileTabs = () => {
               Database
             </button>
             <button
-              className={`py-3 px-0 w-36 uppercase rounded-md border border-[#c3c3c3] text-[#696969] font-[500] text-[14px] transition-all duration-300 
+              className={`py-3 px-0 w-36 uppercase rounded-sm border border-[#c3c3c3] text-[#696969] font-[500] text-[14px] transition-all duration-300 
                 ${activeTab === 3 ? 'bg-[#43b7c5] text-white border-none' : 'bg-white'}
                 focus:bg-[#43b7c5] focus:text-white`}
               onClick={() => setActiveTab(3)}
@@ -51,7 +46,7 @@ const MobileTabs = () => {
               Backend
             </button>
             <button
-              className={`py-3 px-0 w-36 uppercase rounded-md border border-[#c3c3c3] text-[#696969] font-[500] text-[14px] transition-all duration-300 
+              className={`py-3 px-0 w-36 uppercase rounded-sm border border-[#c3c3c3] text-[#696969] font-[500] text-[14px] transition-all duration-300 
                 ${activeTab === 4 ? 'bg-[#43b7c5] text-white border-none' : 'bg-white'}
                 focus:bg-[#43b7c5] focus:text-white`}
               onClick={() => setActiveTab(4)}
@@ -59,7 +54,7 @@ const MobileTabs = () => {
               CMS
             </button>
             <button
-              className={`py-3 px-0 w-36 uppercase rounded-md border border-[#c3c3c3] text-[#696969] font-[500] text-[14px] transition-all duration-300 
+              className={`py-3 px-0 w-36 uppercase rounded-sm border border-[#c3c3c3] text-[#696969] font-[500] text-[14px] transition-all duration-300 
                 ${activeTab === 5 ? 'bg-[#43b7c5] text-white border-none' : 'bg-white'}
                 focus:bg-[#43b7c5] focus:text-white`}
               onClick={() => setActiveTab(5)}
@@ -67,76 +62,76 @@ const MobileTabs = () => {
               Devops
             </button>
           </div>
-        </div>
-
+        
         {/* Tab Content */}
-        <div className="mt-6">
-          {activeTab === 0 && (
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-20 md:gap-x-0 gap-0 mt-8">
-              {tabMobile.map((data, index) => (
-                <div key={index} className="flex flex-col items-center md:px-6 md:hover:-translate-y-2 transition duration-700">
-                  <img className="bg-[#F4F6F6] rounded-lg max-w-64 h-24 md:max-w-96 md:h-36" src={data.icon} alt={data.brand} />
-                  <div className="text-[16px] font-[500] text-[#333333] pt-4 pb-12">{data.brand}</div>
-                </div>
-              ))}
-            </div>
-          )}
+          <div className="mt-6">
+            {activeTab === 0 && (
+              <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-10 gap-0 mt-8 px-4">
+                {tabMobile.map((data, index) => (
+                  <div key={index} className="flex flex-col items-center md:px-6 md:hover:-translate-y-2 transition duration-700">
+                    <img className="bg-[#F4F6F6] rounded-lg max-w-44 h-24" src={data.icon} alt={data.brand} />
+                    <div className="text-[16px] font-[500] text-[#333333] pt-4 pb-12">{data.brand}</div>
+                  </div>
+                ))}
+              </div>
+            )}
 
-          {activeTab === 1 && (
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-20 md:gap-x-0 gap-0 mt-8">
-              {tabFrontEnd.map((data, index) => (
-                <div key={index} className="flex flex-col items-center md:px-6 md:hover:-translate-y-2 transition duration-700">
-                  <img className="bg-[#F4F6F6] rounded-lg max-w-64 h-24 md:max-w-96 md:h-36" src={data.icon} alt={data.brand} />
-                  <div className="text-[16px] font-[500] text-[#333333] pt-4 pb-12">{data.brand}</div>
-                </div>
-              ))}
-            </div>
-          )}
+            {activeTab === 1 && (
+              <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-10 gap-0 mt-8 px-4">
+                {tabFrontEnd.map((data, index) => (
+                  <div key={index} className="flex flex-col items-center md:px-6 md:hover:-translate-y-2 transition duration-700">
+                    <img className="bg-[#F4F6F6] rounded-lg max-w-44 h-24" src={data.icon} alt={data.brand} />
+                    <div className="text-[16px] font-[500] text-[#333333] pt-4 pb-12">{data.brand}</div>
+                  </div>
+                ))}
+              </div>
+            )}
 
-          {activeTab === 2 && (
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-20 md:gap-x-0 gap-0 mt-8">
-              {tabDatabase.map((data, index) => (
-                <div key={index} className="flex flex-col items-center md:px-6 md:hover:-translate-y-2 transition duration-700">
-                  <img className="bg-[#F4F6F6] rounded-lg max-w-64 h-24 md:max-w-96 md:h-36" src={data.icon} alt={data.brand} />
-                  <div className="text-[16px] font-[500] text-[#333333] pt-4 pb-12">{data.brand}</div>
-                </div>
-              ))}
-            </div>
-          )}
+            {activeTab === 2 && (
+              <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-10 gap-0 mt-8 px-4">
+                {tabDatabase.map((data, index) => (
+                  <div key={index} className="flex flex-col items-center md:px-6 md:hover:-translate-y-2 transition duration-700">
+                    <img className="bg-[#F4F6F6] rounded-lg max-w-44 h-24" src={data.icon} alt={data.brand} />
+                    <div className="text-[16px] font-[500] text-[#333333] pt-4 pb-12">{data.brand}</div>
+                  </div>
+                ))}
+              </div>
+            )}
 
-          {activeTab === 3 && (
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-20 md:gap-x-0 gap-0 mt-8">
-              {tabBackEnd.map((data, index) => (
-                <div key={index} className="flex flex-col items-center md:px-6 md:hover:-translate-y-2 transition duration-700">
-                  <img className="bg-[#F4F6F6] rounded-lg max-w-64 h-24 md:max-w-96 md:h-36" src={data.icon} alt={data.brand} />
-                  <div className="text-[16px] font-[500] text-[#333333] pt-4 pb-12">{data.brand}</div>
-                </div>
-              ))}
-            </div>
-          )}
+            {activeTab === 3 && (
+              <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-10 gap-0 mt-8 px-4">
+                {tabBackEnd.map((data, index) => (
+                  <div key={index} className="flex flex-col items-center md:px-6 md:hover:-translate-y-2 transition duration-700">
+                    <img className="bg-[#F4F6F6] rounded-lg max-w-44 h-24" src={data.icon} alt={data.brand} />
+                    <div className="text-[16px] font-[500] text-[#333333] pt-4 pb-12">{data.brand}</div>
+                  </div>
+                ))}
+              </div>
+            )}
 
-            {activeTab === 4 && (
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-20 md:gap-x-0 gap-0 mt-8">
-              {tabCMS.map((data, index) => (
-                <div key={index} className="flex flex-col items-center md:px-6 md:hover:-translate-y-2 transition duration-700">
-                  <img className="bg-[#F4F6F6] rounded-lg max-w-64 h-24 md:max-w-96 md:h-36" src={data.icon} alt={data.brand} />
-                  <div className="text-[16px] font-[500] text-[#333333] pt-4 pb-12">{data.brand}</div>
-                </div>
-              ))}
-            </div>
-          )}
+              {activeTab === 4 && (
+              <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-10 gap-0 mt-8 px-4">
+                {tabCMS.map((data, index) => (
+                  <div key={index} className="flex flex-col items-center md:px-6 md:hover:-translate-y-2 transition duration-700">
+                    <img className="bg-[#F4F6F6] rounded-lg max-w-44 h-24" src={data.icon} alt={data.brand} />
+                    <div className="text-[16px] font-[500] text-[#333333] pt-4 pb-12">{data.brand}</div>
+                  </div>
+                ))}
+              </div>
+            )}
 
-            {activeTab === 5 && (
-            <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-20 md:gap-x-0 gap-0 mt-8">
-              {tabDevops.map((data, index) => (
-                <div key={index} className="flex flex-col items-center md:px-6 md:hover:-translate-y-2 transition duration-700">
-                  <img className="bg-[#F4F6F6] rounded-lg max-w-64 h-24 md:max-w-96 md:h-36" src={data.icon} alt={data.brand} />
-                  <div className="text-[16px] font-[500] text-[#333333] pt-4 pb-12">{data.brand}</div>
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
+              {activeTab === 5 && (
+              <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-10 gap-0 mt-8 px-4">
+                {tabDevops.map((data, index) => (
+                  <div key={index} className="flex flex-col items-center md:px-6 md:hover:-translate-y-2 transition duration-700">
+                    <img className="bg-[#F4F6F6] rounded-lg max-w-44 h-24" src={data.icon} alt={data.brand} />
+                    <div className="text-[16px] font-[500] text-[#333333] pt-4 pb-12">{data.brand}</div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+       
       </div>
     </div>
   );
